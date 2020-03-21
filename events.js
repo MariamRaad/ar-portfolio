a7bc0d6bff6ab1361c7b198536f6c2692f0574af
Mariam <mariam.raad@gmx.de>
2020-03-21 17:55:22 +0100

    changed IDs

--- events.js
+++ events.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent('markerhandler', {
 
     init: function() {
         const animatedMarker = document.querySelector("#animated-marker");
-        const aEntity = document.querySelector("#animated-model");
+        const aEntity = document.querySelector("#redBox-scaling");
 
         // every click, we make our model grow in size :)
         animatedMarker.addEventListener('click', function(ev, target){
