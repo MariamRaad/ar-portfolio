53ce0fe8ec414cb2654cdbbf11553a047033398b
Mariam <mariam.raad@gmx.de>
2020-06-02 15:20:54 +0200

    added two new functions
    navigateOnClick to handle click and forwarding an URL
    and
    colorRandomizer to change color of an object

--- events.js
+++ events.js
@@ -1,3 +1,33 @@
+/* To add interactivity to the site with Javascript */
+
+AFRAME.registerComponent('navigate-on-click', {
+  schema: {
+    url: {
+      default: ''
+    }
+  },
+  init: function () {
+    console.log("hello")
+    console.log(this.el)
+    var data = this.data;
+    var el = this.el;
+    el.addEventListener('click', function () {
+      //window.location.href = data.url;
+      window.open(data.url, '_blank');
+    });
+  }
+});
+
+AFRAME.registerComponent('color-randomizer', {
+  init: function () {
+    let colors = ["red", "green", "blue", "black", "orange", "white"]
+    var el = this.el;
+    el.addEventListener('click', (e) => {     
+      this.el.setAttribute('color', colors[Math.floor(Math.random() * colors.length)])
+    });
+  }
+});
+
 AFRAME.registerComponent('markerhandler', {
 
     init: function() {
