020aa948194e825e5364e5c2bcebb0044ca00334
Mariam <mariam.raad@gmx.de>
2020-06-03 13:23:57 +0200

    added videohandler

--- events.js
+++ events.js
@@ -44,3 +44,26 @@ AFRAME.registerComponent('markerhandler', {
             }
         });
 }});
+
+ //window.onload = function() {
+   AFRAME.registerComponent("videohandler", {
+     init: function() {
+       var marker = this.el;
+       this.vid = document.querySelector("#vid");
+       marker.addEventListener(
+         "markerFound",
+         function(){
+           this.vid.play();
+         }.bind(this)
+       );
+                                
+       marker.addEventListener(
+         "markerLost",
+         function() {
+           this.vid.pause();
+           this.vid.currentTime = 0;
+         }.bind(this)
+       );
+     }
+   });
+ //};
