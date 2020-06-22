70cd7cadd90fadf1061e914157f3ecc4bbf7a763
Mariam <mariam.raad@gmx.de>
2020-06-22 11:39:47 +0200

    Update gesture-handler.js

--- gesture-handler.js
+++ gesture-handler.js
@@ -34,19 +34,19 @@ AFRAME.registerComponent("gesture-handler", {
 
   update: function () {
     if (this.data.enabled) {
-      this.video.addEventListener("onefingerstart", this.handleClick);
+      this.el.object3D.addEventListener("onefingerstart", this.handleClick);
       //this.el.sceneEl.addEventListener("onefingerstart", this.handleClick);
       //this.el.sceneEl.addEventListener("onefingermove", this.handleRotation);
       //this.el.sceneEl.addEventListener("twofingermove", this.handleScale);
     } else {
-      this.video.removeEventListener("onefingerstart", this.handleClick);
+      this.el.object3D.removeEventListener("onefingerstart", this.handleClick);
       //this.el.sceneEl.removeEventListener("onefingermove", this.handleRotation);
       //this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
     }
   },
 
   remove: function () {
-    this.video.removeEventListener("onefingerstart", this.handleClick);
+    this.el.object3D.removeEventListener("onefingerstart", this.handleClick);
     //this.el.sceneEl.removeEventListener("onefingerend", this.handleScale);
   },
 
