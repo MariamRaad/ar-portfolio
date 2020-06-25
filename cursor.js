3cd3ab04de65ea0b2ebc161ac41a01298baa99e0
Mariam <mariam.raad@gmx.de>
2020-06-25 17:32:00 +0200

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -4,6 +4,7 @@
 AFRAME.registerComponent('videohandler_1', {
 	init: function() {
 		// the clicks may fire prematurely for some reason ¯\_(ツ)_/¯
+		// TODO: is there any disadvantage that it fires prematurely?
 		this.el.sceneEl.addEventListener("arSessionReady", this.addListeners.call(this));
 	},
 	addListeners: function() {
@@ -49,7 +50,8 @@ AFRAME.registerComponent('scannerhandler', {
   	}
 });
 
-/* Ar-session-notifier Function: to set a flag when the arSession is ready */
+/* Ar-session-notifier Function: to set a flag when the arSession is ready 
+   "ready"-state means: when other components can access the system, and use the ar.js core. */
 AFRAME.registerComponent('ar-session-notifier', {
 	init: function() {
 		var scene = this.el.sceneEl
@@ -57,7 +59,7 @@ AFRAME.registerComponent('ar-session-notifier', {
 		// wait until the arSession is ready
 		var idx = setInterval(function() {
 			arSession = scene.systems["arjs"]._arSession
-			if (!arSession) return;
+			if (!arSession) return; // It just checks when the _arSession is not undefined, or null - and emits a signal.
 			scene.emit("arSessionReady")
 			clearInterval(idx)
 		})
@@ -82,28 +84,30 @@ AFRAME.registerComponent('cursor-hack', {
 
 			function mousedown(event) {
 				// core of this 'hack' - using the arToolkitContext projection matrix
+				// makes sure that jsartoolkit5 projection matrix is not out of sync with the threejs projection matrix
 				camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
 				camera.projectionMatrixInverse.getInverse(camera.projectionMatrix);
 
 				var point
 				if (event.type === "touchmove" || event.type === "touchstart") {
 					// Track the first touch for simplicity.
+					// TODO: why does it need to be tracked? to align the matrices?
 					point = event.touches.item(0);
 				} else {
 					point = event;
 				}
 				// Calculate mouse position based on the canvas element
-				var rect = scene.renderer.domElement.getBoundingClientRect();
-				mouse.x = ((point.clientX - rect.left) / rect.width) * 2 - 1
-				mouse.y = -((point.clientY - rect.top) / rect.height) * 2 + 1
-				raycaster.setFromCamera(mouse, camera);
+				var rect = scene.renderer.domElement.getBoundingClientRect(); // TODO: are that just the boundaries/borders of the canvas?
+				mouse.x = ((point.clientX - rect.left) / rect.width) * 2 - 1  // TODO: what does this do?
+				mouse.y = -((point.clientY - rect.top) / rect.height) * 2 + 1 // TODO: what does this do?
+				raycaster.setFromCamera(mouse, camera); 		      // TODO: what does this do?
 				// if there are any intersections - send the clicks
 				var intersects = raycaster.intersectObjects(scene.object3D.children, true);
 				if (intersects.length > 0) {
 					// this click is stripped of any info it should have
-					intersects[0].object.el.emit("click")
+					intersects[0].object.el.emit("click") // TODO: what other info would it have?
 				}
-				event.stopPropagation();
+				event.stopPropagation(); // TODO: why does it need to stop?
 			}
 			window.addEventListener('mousedown', mousedown, false);
 			//window.addEventListener('touchstart', mousedown, false);
