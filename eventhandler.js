11a29819de3d0184c79b379229caf994095f54e3
Mariam <mariam.raad@gmx.de>
2020-06-27 18:33:19 +0200

    Update eventhandler.js

--- eventhandler.js
+++ eventhandler.js
@@ -23,13 +23,13 @@ AFRAME.registerComponent("videohandler", {
 		marker.addEventListener("markerFound", function () {
 			this.video_src_1.pause(); //needs to be set or otherwise the video will start automatically
 			isVideoPlaying = false;
+			
 			//wenn wir in Szene 1 sind (bei Video1) und das Video vom User bereits abgespielt wurde und das Video zwischenzeitlich pausiert hat (weil Marker verloren)
-       			if ( (this.video_plane_1.getAttribute("visible") == true) && (this.button_play_1.getAttribute("visible") == false) && (!isVideoPlaying) ) {
+       			if ( ((this.video_plane_1.getAttribute("visible") == true) && (this.button_play_1.getAttribute("visible") == false)) && (isVideoPlaying == false) ) {
 			// if (this.video_plane_1.getAttribute("visible") == true && !isVideoPlaying)
 				this.video_src_1.play();
 			 	isVideoPlaying = true;
 			}
-				
     		}.bind(this));
     
    		 marker.addEventListener("markerLost", function () {
@@ -46,7 +46,6 @@ AFRAME.registerComponent("videohandler", {
 			*/
     		}.bind(this));
 			
-		
 		this.el.addEventListener("click", e => {
 			if (this.el === this.button_play_1) {
 				this.button_play_1.setAttribute("visible", false);
